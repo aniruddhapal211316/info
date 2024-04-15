acaa5db185463c6544199c610f61514d12089bc0
aniruddhapal211316 <aniruddhapal211316@gmail.com>
2024-04-15 08:35:08 +0530

    CMGAN multi gpu

---
+++ CMGAN/src/train.py
@@ -0,0 +1,317 @@
+from models.generator import TSCNet
+from models import discriminator
+import os
+from data import dataloader
+import torch.nn.functional as F
+import torch
+from utils import power_compress, power_uncompress
+import logging
+from torchinfo import summary
+import argparse
+
+import torch.multiprocessing as mp
+from torch.nn.parallel import DistributedDataParallel as DDP
+# from torch.distributed import init_process_group, destroy_process_group
+import torch.distributed as dist
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--epochs", type=int, default=120, help="number of epochs of training")
+parser.add_argument("--batch_size", type=int, default=4)
+parser.add_argument("--log_interval", type=int, default=500)
+parser.add_argument("--decay_epoch", type=int, default=30, help="epoch from which to start lr decay")
+parser.add_argument("--init_lr", type=float, default=5e-4, help="initial learning rate")
+parser.add_argument("--cut_len", type=int, default=16000*2, help="cut length, default is 2 seconds in denoise "
+                                                                 "and dereverberation")
+parser.add_argument("--data_dir", type=str, default='dir to VCTK-DEMAND dataset',
+                    help="dir of VCTK+DEMAND dataset")
+parser.add_argument("--save_model_dir", type=str, default='./saved_model',
+                    help="dir of saved model")
+parser.add_argument("--loss_weights", type=list, default=[0.1, 0.9, 0.2, 0.05],
+                    help="weights of RI components, magnitude, time loss, and Metric Disc")
+args = parser.parse_args()
+logging.basicConfig(level=logging.INFO)
+
+class Trainer:
+    def __init__(self, train_ds, test_ds, gpu_id):
+        self.n_fft = 400
+        self.hop = 100
+        self.train_ds = train_ds
+        self.test_ds = test_ds
+        self.gpu_id = gpu_id
+        self.epochs_run = 0
+        self.snapshot_path = os.path.join(args.save_model_dir, 'snapshot.pt')
+        self.model = TSCNet(num_channel=64, num_features=self.n_fft // 2 + 1).to(self.gpu_id)
+        self.model = DDP(self.model, device_ids=[self.gpu_id])
+        summary(
+            self.model, [(1, 2, args.cut_len // self.hop + 1, int(self.n_fft / 2) + 1)]
+        )
+        self.discriminator = discriminator.Discriminator(ndf=16).to(self.gpu_id)
+        self.discriminator = DDP(self.discriminator, device_ids=[self.gpu_id])
+        summary(
+            self.discriminator,
+            [
+                (1, 1, int(self.n_fft / 2) + 1, args.cut_len // self.hop + 1),
+                (1, 1, int(self.n_fft / 2) + 1, args.cut_len // self.hop + 1),
+            ],
+        )
+        self.optimizer = torch.optim.AdamW(self.model.parameters(), lr=args.init_lr)
+        self.optimizer_disc = torch.optim.AdamW(
+            self.discriminator.parameters(), lr=2 * args.init_lr
+        )
+        self.scheduler_G = torch.optim.lr_scheduler.StepLR(
+            self.optimizer, step_size=args.decay_epoch, gamma=0.5
+        )
+        self.scheduler_D = torch.optim.lr_scheduler.StepLR(
+            self.optimizer_disc, step_size=args.decay_epoch, gamma=0.5
+        )
+        if os.path.exists(self.snapshot_path): 
+            self._load_snapshot()
+
+    def forward_generator_step(self, clean, noisy):
+
+        # Normalization
+        c = torch.sqrt(noisy.size(-1) / torch.sum((noisy**2.0), dim=-1))
+        noisy, clean = torch.transpose(noisy, 0, 1), torch.transpose(clean, 0, 1)
+        noisy, clean = torch.transpose(noisy * c, 0, 1), torch.transpose(
+            clean * c, 0, 1
+        )
+
+        noisy_spec = torch.stft(
+            noisy,
+            self.n_fft,
+            self.hop,
+            window=torch.hamming_window(self.n_fft).to(self.gpu_id),
+            onesided=True,
+        )
+        clean_spec = torch.stft(
+            clean,
+            self.n_fft,
+            self.hop,
+            window=torch.hamming_window(self.n_fft).to(self.gpu_id),
+            onesided=True,
+        )
+        noisy_spec = power_compress(noisy_spec).permute(0, 1, 3, 2)
+        clean_spec = power_compress(clean_spec)
+        clean_real = clean_spec[:, 0, :, :].unsqueeze(1)
+        clean_imag = clean_spec[:, 1, :, :].unsqueeze(1)
+
+        est_real, est_imag = self.model(noisy_spec)
+        est_real, est_imag = est_real.permute(0, 1, 3, 2), est_imag.permute(0, 1, 3, 2)
+        est_mag = torch.sqrt(est_real**2 + est_imag**2)
+        clean_mag = torch.sqrt(clean_real**2 + clean_imag**2)
+
+        est_spec_uncompress = power_uncompress(est_real, est_imag).squeeze(1)
+        est_audio = torch.istft(
+            est_spec_uncompress,
+            self.n_fft,
+            self.hop,
+            window=torch.hamming_window(self.n_fft).to(self.gpu_id),
+            onesided=True,
+        )
+
+        return {
+            "est_real": est_real,
+            "est_imag": est_imag,
+            "est_mag": est_mag,
+            "clean_real": clean_real,
+            "clean_imag": clean_imag,
+            "clean_mag": clean_mag,
+            "est_audio": est_audio,
+        }
+
+    def calculate_generator_loss(self, generator_outputs):
+
+        predict_fake_metric = self.discriminator(
+            generator_outputs["clean_mag"], generator_outputs["est_mag"]
+        )
+        gen_loss_GAN = F.mse_loss(
+            predict_fake_metric.flatten(), generator_outputs["one_labels"].float()
+        )
+
+        loss_mag = F.mse_loss(
+            generator_outputs["est_mag"], generator_outputs["clean_mag"]
+        )
+        loss_ri = F.mse_loss(
+            generator_outputs["est_real"], generator_outputs["clean_real"]
+        ) + F.mse_loss(generator_outputs["est_imag"], generator_outputs["clean_imag"])
+
+        time_loss = torch.mean(
+            torch.abs(generator_outputs["est_audio"] - generator_outputs["clean"])
+        )
+
+        loss = (
+            args.loss_weights[0] * loss_ri
+            + args.loss_weights[1] * loss_mag
+            + args.loss_weights[2] * time_loss
+            + args.loss_weights[3] * gen_loss_GAN
+        )
+
+        return loss
+
+    def calculate_discriminator_loss(self, generator_outputs):
+
+        length = generator_outputs["est_audio"].size(-1)
+        est_audio_list = list(generator_outputs["est_audio"].detach().cpu().numpy())
+        clean_audio_list = list(generator_outputs["clean"].cpu().numpy()[:, :length])
+        pesq_score = discriminator.batch_pesq(clean_audio_list, est_audio_list)
+
+        # The calculation of PESQ can be None due to silent part
+        if pesq_score is not None:
+            predict_enhance_metric = self.discriminator(
+                generator_outputs["clean_mag"], generator_outputs["est_mag"].detach()
+            )
+            predict_max_metric = self.discriminator(
+                generator_outputs["clean_mag"], generator_outputs["clean_mag"]
+            )
+            discrim_loss_metric = F.mse_loss(
+                predict_max_metric.flatten(), generator_outputs["one_labels"]
+            ) + F.mse_loss(predict_enhance_metric.flatten(), pesq_score)
+        else:
+            discrim_loss_metric = None
+
+        return discrim_loss_metric
+
+    def train_step(self, batch):
+
+        # Trainer generator
+        clean = batch[0].to(self.gpu_id)
+        noisy = batch[1].to(self.gpu_id)
+        one_labels = torch.ones(args.batch_size).to(self.gpu_id)
+
+        generator_outputs = self.forward_generator_step(
+            clean,
+            noisy,
+        )
+        generator_outputs["one_labels"] = one_labels
+        generator_outputs["clean"] = clean
+
+        loss = self.calculate_generator_loss(generator_outputs)
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        # Train Discriminator
+        discrim_loss_metric = self.calculate_discriminator_loss(generator_outputs)
+
+        if discrim_loss_metric is not None:
+            self.optimizer_disc.zero_grad()
+            discrim_loss_metric.backward()
+            self.optimizer_disc.step()
+        else:
+            discrim_loss_metric = torch.tensor([0.0])
+
+        return loss.item(), discrim_loss_metric.item()
+
+    @torch.no_grad()
+    def test_step(self, batch):
+
+        clean = batch[0].to(self.gpu_id)
+        noisy = batch[1].to(self.gpu_id)
+        one_labels = torch.ones(args.batch_size).to(self.gpu_id)
+
+        generator_outputs = self.forward_generator_step(
+            clean,
+            noisy,
+        )
+        generator_outputs["one_labels"] = one_labels
+        generator_outputs["clean"] = clean
+
+        loss = self.calculate_generator_loss(generator_outputs)
+
+        discrim_loss_metric = self.calculate_discriminator_loss(generator_outputs)
+        if discrim_loss_metric is None:
+            discrim_loss_metric = torch.tensor([0.0])
+
+        return loss.item(), discrim_loss_metric.item()
+
+    def test(self):
+        self.model.eval()
+        self.discriminator.eval()
+        gen_loss_total = 0.0
+        disc_loss_total = 0.0
+        for idx, batch in enumerate(self.test_ds):
+            step = idx + 1
+            loss, disc_loss = self.test_step(batch)
+            gen_loss_total += loss
+            disc_loss_total += disc_loss
+        gen_loss_avg = gen_loss_total / step
+        disc_loss_avg = disc_loss_total / step
+
+        template = "GPU: {}, Generator loss: {}, Discriminator loss: {}"
+        logging.info(template.format(self.gpu_id, gen_loss_avg, disc_loss_avg))
+
+        return gen_loss_avg
+
+    def train(self):
+        
+        for epoch in range(self.epochs_run, args.epochs):
+            self.model.train()
+            self.discriminator.train()
+            for idx, batch in enumerate(self.train_ds):
+                step = idx + 1
+                loss, disc_loss = self.train_step(batch)
+                template = "GPU: {}, Epoch {}, Step {}, loss: {}, disc_loss: {}"
+                if (step % args.log_interval) == 0:
+                    logging.info(
+                        template.format(self.gpu_id, epoch, step, loss, disc_loss)
+                    )
+            gen_loss = self.test()
+            path = os.path.join(
+                args.save_model_dir,
+                "CMGAN_epoch_" + str(epoch) + "_" + str(gen_loss)[:5],
+            )
+            if not os.path.exists(args.save_model_dir):
+                os.makedirs(args.save_model_dir)
+            if self.gpu_id == 0:
+                torch.save(self.model.module.state_dict(), path)
+                print(f'Epoch {epoch} | Training model saved at {self.snapshot_path}')
+                self._save_snapshot(epoch)
+            self.scheduler_G.step()
+            self.scheduler_D.step()
+        os.remove(self.snapshot_path)
+        print(f'Removed training snapshot saved from {self.snapshot_path}')
+
+    def _save_snapshot(self, epoch): 
+        snapshot = dict()
+        snapshot['GENERATOR_STATE'] = self.model.module.state_dict()
+        snapshot['DISCRIMINATOR_STATE'] = self.discriminator.module.state_dict()
+        snapshot['GENERATOR_OPT_STATE'] = self.optimizer.state_dict()
+        snapshot['DISCRIMINATOR_OPT_STATE'] = self.optimizer_disc.state_dict()
+        snapshot['SCHEDULER_G_STATE'] = self.scheduler_G.state_dict()
+        snapshot['SCHEDULER_D_STATE'] = self.scheduler_D.state_dict()
+        snapshot['EPOCHS_RUN'] = epoch
+        torch.save(snapshot, self.snapshot_path)
+        print(f'Epoch {epoch} | Training snapshot saved at {self.snapshot_path}')
+
+    def _load_snapshot(self): 
+        snapshot = torch.load(self.snapshot_path)
+        self.model.load_state_dict(snapshot['GENERATOR_STATE'])
+        self.discriminator.load_state_dict(snapshot['DISCRIMINATOR_STATE'])
+        self.optimizer.load_state_dict(snapshot['GENERATOR_OPT_STATE'])
+        self.optimizer_disc.load_state_dict(snapshot['DISCRIMINATOR_OPT_STATE'])
+        self.scheduler_G.load_state_dict(snapshot['SCHEDULER_G_STATE'])
+        self.scheduler_D.load_state_dict(snapshot['SCHEDULER_D_STATE'])
+        self.epochs_run = snapshot['EPOCHS_RUN']
+        print(f'Loaded the recent snapshot of epoch {self.epochs_run} from {self.snapshot_path}')
+
+# def main(rank: int, world_size: int, args):
+def main():
+    dist.init_process_group("nccl" if torch.distributed.is_nccl_available() else "gloo")
+    rank = dist.get_rank()
+    gpu_id = rank % torch.cuda.device_count()
+    if gpu_id == 0:
+        print(f'The arguments passed are : {args}')
+        available_gpus = [
+            torch.cuda.get_device_name(i) for i in range(torch.cuda.device_count())
+        ]
+        print(f'The available GPUS are : {available_gpus}')
+    train_ds, test_ds = dataloader.load_data(
+        args.data_dir, args.batch_size, mp.cpu_count(), args.cut_len
+    )
+    trainer = Trainer(train_ds, test_ds, gpu_id)
+    trainer.train()
+    dist.destroy_process_group()
+
+
+if __name__ == "__main__":
+    main()
